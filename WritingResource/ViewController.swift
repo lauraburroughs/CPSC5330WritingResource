62a0464431434f0674f3c1af2984787118df4ffa
Laura Burroughs <hellolauraburroughs@gmail.com>
2026-02-12 06:35:13 -0600

    Initial Commit

---
+++ WritingResource/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WritingResource
+//
+//  Created by Laura Burroughs on 2/12/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
